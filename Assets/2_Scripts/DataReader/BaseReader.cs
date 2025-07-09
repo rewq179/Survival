07814617e676f39805257ff493f38e4de5d78def
HaeSol <rewq179@gmail.com>
2025-07-09 13:57:27 +0900

    주소 제거

--- Assets/2_Scripts/DataReader/BaseReader.cs
+++ Assets/2_Scripts/DataReader/BaseReader.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 
 public abstract class BaseReader : ScriptableObject
 {
-    public string sheetAddress = "15RA8mCckMbGIXxx5Ukfmtih0TxDTEGkXiAVyrHf8Blw";
+    public string sheetAddress = "";
     public abstract string sheetName { get; }
     [Header("읽기 시작할 행 번호")][SerializeField] public int startRow = 2;
     [Header("읽을 마지막 행 번호")][SerializeField] public int endRow = -1;
